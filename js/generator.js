8721efd2f1dd7687c458bacc92d055a5a0002071
Vincent Liuss <liusvinv@gmail.com>
2022-11-17 03:58:31 +0700

    Add code generator function

--- js/generator.js
+++ js/generator.js
@@ -10,5 +10,64 @@ btnEl.addEventListener("click", ()=>{
 
 // Main Generator Function
 function generateCode(){
-    return 177013;
+    // return 177013;
+
+    // Base Number Variables
+    let [num1,num2,num3,num4,num5,num6] = 
+    [ 
+        getRandomRange(4),  // 1    0 <=> 4
+        getRandomRange(9),  // 2    0 <=> 9
+        getRandomRange(9),  // 3    0 <=> 9    Default Range Random between 0-9
+        getRandomRange(9),  // 4    0 <=> 9      
+        getRandomRange(9),  // 5    0 <=> 9
+        getRandomRange(9)   // 6    0 <=> 9
+
+    ];
+
+
+    // Generate Rules And Exception
+    if(num1 === 4){
+        num2 = getRandomRange(2);
+        
+        if(num2 === 2){
+            num3 = getRandomRange(8);
+
+            if(num3 === 8){
+                num4 = getRandomRange(2);
+                
+                if(num4 === 2){
+                    num5 = 0;
+                    num6 = 0;
+                }
+            }
+
+        }
+
+    }
+
+ 
+
+    // Concat all of the generated numbers and return it
+    let generatedNumber = concatNumber(num1,num2,num3,num4,num5,num6);
+    generatedNumber = parseInt(generatedNumber,10);
+
+    return generatedNumber;
+}
+
+// Generate a number between range
+function getRandomRange(max){
+    max++; // Increment so the max become inclusive
+    return Math.floor(Math.random() * max);
+}
+
+// Function to concat a number in a string way
+function concatNumber(...nums){
+
+    let result = '';
+
+    nums.forEach(number => {
+        result += number;
+    });
+
+    return result;
 }
